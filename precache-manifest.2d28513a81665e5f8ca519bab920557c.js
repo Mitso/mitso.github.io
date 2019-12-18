875f532fc59907c0c1e46ef34c615f5bc51e6f79
Mitso Qalaba <mqalaba@gmail.com>
2019-12-18 15:27:45 +0200

    Inintial buil project commit

---
+++ precache-manifest.2d28513a81665e5f8ca519bab920557c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bc41dec129a59ab8f071bccd35e613a",
+    "url": "/Mitso/Mitso.github.io/index.html"
+  },
+  {
+    "revision": "8944a547e52383216350",
+    "url": "/Mitso/Mitso.github.io/static/css/main.d94ce968.chunk.css"
+  },
+  {
+    "revision": "d8fb6bbe6cf646efdf96",
+    "url": "/Mitso/Mitso.github.io/static/js/2.37620a93.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Mitso/Mitso.github.io/static/js/2.37620a93.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8944a547e52383216350",
+    "url": "/Mitso/Mitso.github.io/static/js/main.5689982d.chunk.js"
+  },
+  {
+    "revision": "0218115306dd171b40c4",
+    "url": "/Mitso/Mitso.github.io/static/js/runtime-main.f29cb956.js"
+  }
+]);
