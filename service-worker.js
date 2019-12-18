a2e2e14b5962ba443737f26f0cbe0979986852b9
Mitso Qalaba <mqalaba@gmail.com>
2019-12-18 15:37:40 +0200

    Inintial buil project commit

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Mitso/Mitso.github.io/precache-manifest.2d28513a81665e5f8ca519bab920557c.js"
+  "/precache-manifest.98f5f2f702b59ffc908cad1f3cead4ae.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Mitso/Mitso.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
