bedd9a1a37b84b4d1c503e2b76bb30671d85f4ef
Mitso Qalaba <mqalaba@gmail.com>
2020-01-22 09:35:58 +0200

    update app ui

---
+++ precache-manifest.046ca4350d90fd4afd69712b514a7651.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ed1d4bc2ec0770a106445af7aa11419",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fe255833bd8931f854c",
+    "url": "/static/css/main.7da27907.chunk.css"
+  },
+  {
+    "revision": "8724610ae275f91a963d",
+    "url": "/static/js/2.575883f8.chunk.js"
+  },
+  {
+    "revision": "8431cf56751069dc7fa59c771a271cba",
+    "url": "/static/js/2.575883f8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8fe255833bd8931f854c",
+    "url": "/static/js/main.0c49910a.chunk.js"
+  },
+  {
+    "revision": "caa0f2c14e18d2c77fa2",
+    "url": "/static/js/runtime-main.5274d17f.js"
+  },
+  {
+    "revision": "baede6d4a9e73953df24cd6654b4d4c8",
+    "url": "/static/media/innovative-thoughts-1.baede6d4.jpg"
+  },
+  {
+    "revision": "b2da18c73b949c6742b772891e09dbb6",
+    "url": "/static/media/ndoh-logo.b2da18c7.png"
+  }
+]);
