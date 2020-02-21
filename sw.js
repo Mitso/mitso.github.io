9e056cd8aa05e20246ebae536fe3e4a8617ff346
Mitso Qalaba <mqalaba@gmail.com>
2020-02-21 13:06:26 +0200

    update sw generate

--- sw.js
+++ sw.js
@@ -1,10 +1,46 @@
 importScripts("/precache-manifest.f169daa91ab4c96d46edaeac8788698f.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-// Global workbox
-console.log(workbox);
+  // Global workbox
   if (workbox) {
     // Disable logging
     console.log("Workbox is loaded");
+    workbox.setConfig({ debug: true });
+
+    // Add an event listener to detect when the registered
+    // service worker has installed but is waiting to activate
+    console.log(self);
+    self.addEventListener("waiting", (event) => {
+      // `event.wasWaitingBeforeRegister` will be false if this is
+      // the first time the updated service worker is waiting.
+      // When `event.wasWaitingBeforeRegister` is true, a previously
+      // updated same service worker is still waiting.
+      // You may want to customize the UI prompt accordingly.
+      const prompt = createUIPrompt({
+        onAccept: async () => {
+          // Assuming the user accepted the update, set up a listener
+          // that will reload the page as soon as the previously waiting
+          // service worker has taken control.
+          self.addEventListener("controlling", (event) => {
+            window.location.reload();
+          });
+          // Send a message telling the service worker to skip waiting.
+          // This will trigger the `controlling` event handler above.
+          // Note: for this to work, you have to add a message
+          // listener in your service worker. See below.
+          self.addEventListener("message", (event) => {
+            if(event.data && event.data.type === "SKIP_WAITING"){
+              skipWaiting();
+            }
+          });
+        },
+        onReject: () => {
+          prompt.dismiss();
+        }
+      })
+    });
+
+
+
     workbox.precaching.precacheAndRoute(self.__precacheManifest);
   } else {
     console.error("Workbox could not be loaded. No offline support");
